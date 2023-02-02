ca20448d831336eb62c7d7cfbe457bf61a134af7
Josh Blad <jpb82@byu.edu>
2023-02-01 19:21:38 -0700

    Add project files.

---
+++ Mission6Assignment/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MoviesContext blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DatingApplication()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DatingApplication(MoviesResponse mr)
+        {
+            blahContext.Add(mr);
+            blahContext.SaveChanges();
+
+            return View("Confirmation", mr);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
